4dc93e8f2e556afdce87c622d1a80fd34f476024
Rayansh Gupta <rayanshgupta1406@gmail.com>
2020-08-19 14:35:13 +0530

    completed blog page

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/blogs/")
     date = models.DateField(auto_now=True)
 
     def __str__(self):
--- product/models.py
+++ product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.ImageField(upload_to="images/products")
+    ENVELOPE = 'ENV'
+    GIFT_TAG = 'GTA'
+    WINE_BAG = 'WBA'
+    PRODUCT_CHOICES = [
+        (ENVELOPE, 'Envelope'),
+        (GIFT_TAG, 'Gift Tag'),
+        (WINE_BAG, 'Wine Bag')
+    ]
+    product_type = models.CharField(
+        max_length=3, choices=PRODUCT_CHOICES, default=ENVELOPE)
